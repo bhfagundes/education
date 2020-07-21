de957c8fc1bace24ed8867cec98b6f93bad8985a
Brenner Fagundes <bhfagundes@gmail.com>
2020-07-21 00:53:07 -0300

    migration de cursos

---
+++ database/migrations/2020_07_21_033138_curse.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Curse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+   public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('codigo')->unique();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
